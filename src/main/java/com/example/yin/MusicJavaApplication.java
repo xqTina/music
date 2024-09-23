e80e7b05b7d544792c50d55aeecd14e5c6e0c8d8
xqTina <3508729644@qq.com>
2024-09-23 16:52:33 +0800

    初始化项目

---
+++ src/main/java/com/example/yin/MusicJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.yin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yin/MusicJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

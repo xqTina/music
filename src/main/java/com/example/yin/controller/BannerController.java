c019703a63208c6906a9353d665d76a71e853520
lf <505787045@qq.com>
2024-10-13 19:58:48 +0800

    轮播图优化

--- src/main/java/com/example/yin/controller/BannerController.java
+++ src/main/java/com/example/yin/controller/BannerController.java
@@ -2,12 +2,14 @@ package com.example.yin.controller;
 
 import com.example.yin.common.R;
 import com.example.yin.service.impl.BannerService;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
+@RestController
 @RequestMapping("/banner")
 public class BannerController {
 

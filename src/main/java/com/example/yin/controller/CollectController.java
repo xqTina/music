88701bbc3caec776786f8e5b5c501d31e9106cc2
PENGMIN <3040421192@qq.com>
2024-11-11 10:13:43 +0800

    收藏

---
+++ src/main/java/com/example/yin/controller/CollectController.java
@@ -0,0 +1,73 @@
+package com.example.yin.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.yin.common.R;
+import com.example.yin.model.domin.Collect;
+import com.example.yin.model.request.CommentRequest;
+import com.example.yin.service.impl.CollectService;
+import com.example.yin.utils.Consts;
+import org.checkerframework.checker.units.qual.C;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class CollectController {
+
+    @Autowired
+    private CollectService collectService;
+
+
+    // 提交收藏
+    @PostMapping("/collect/add")
+     public Object addCollect(HttpServletRequest request) {
+        JSONObject jsonObject = new JSONObject();
+        String userId = request.getParameter("userId");
+        String type = request.getParameter("type");
+        String songId = request.getParameter("songId");
+        if (songId == null || songId.equals("")) {
+            jsonObject.put(Consts.CODE, 0);
+            jsonObject.put(Consts.MSG, "收藏歌曲为空");
+            return jsonObject;
+        }
+        if (collectService.existSongId(Integer.parseInt(userId), Integer.parseInt(songId))) {
+            jsonObject.put(Consts.CODE, 2);
+            jsonObject.put(Consts.MSG, "已收藏");
+            return jsonObject;
+        }
+        //保存到收藏对象中
+        Collect collect = new Collect();
+        collect.setUserId(Integer.parseInt(songId));
+        collect.setType(new Byte(type));
+        collect.setSongId(Integer.parseInt(songId));
+        Boolean flag = collectService.insert(collect);
+        if (flag) {
+            jsonObject.put(Consts.CODE, 1);
+            jsonObject.put(Consts.MSG, "收藏成功");
+            return jsonObject;
+        }
+        jsonObject.put(Consts.CODE,0);
+        jsonObject.put(Consts.MSG,"收藏失败");
+        return jsonObject;
+
+    }
+    @RequestMapping(value = "/collect/delete",method = RequestMethod.GET)
+    public Object deleteCollect(HttpServletRequest request){
+        String id = request.getParameter("id").trim();
+        boolean flag = collectService.delete(Integer.parseInt(id));
+        return flag;
+    }
+    @GetMapping("/collect/allCollect")
+    public Object allCollect(HttpServletRequest request){
+        return collectService.allCollect();
+    }
+    /*查询某个用户下的收藏列表*/
+    @GetMapping("/collect/collectOfUserId")
+    public Object collectOfUserId(HttpServletRequest request){
+        String userId = request.getParameter("userId");
+        return collectService.collectOfUserId(Integer.parseInt(userId));
+
+    }
+
+}
